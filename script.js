64f6c3909886c98198ac233189446bd15cbca40c
Mareike Heuer <mareike.heuer@hotmail.com>
2021-05-20 22:27:42 +0200

    Display map with leaflet library

--- script.js
+++ script.js
@@ -20,6 +20,21 @@ if (navigator.geolocation)
       console.log(
         `https://www.google.com/maps/@${latitude},${longitude},13.84z`
       );
+
+      const coords = [latitude, longitude];
+
+      // setView() = 1st argument: latitude, longitude, 2nd argument: map zoom level
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');

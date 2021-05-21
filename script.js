4b52af99283a43116d86fe60f6714c8d7280d08a
Mareike Heuer <mareike.heuer@hotmail.com>
2021-05-21 16:36:42 +0200

    Display a map marker

--- script.js
+++ script.js
@@ -25,16 +25,32 @@ if (navigator.geolocation)
 
       // setView() = 1st argument: latitude, longitude, 2nd argument: map zoom level
       const map = L.map('map').setView(coords, 13);
+      console.log(map);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your position');

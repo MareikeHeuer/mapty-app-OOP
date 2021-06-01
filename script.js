6533de765bb8c2e8fc112e2983134042fb0be1d7
Mareike Heuer <mareike.heuer@hotmail.com>
2021-06-01 11:53:27 +0200

    Manage workout data - create classes

--- script.js
+++ script.js
@@ -11,6 +11,54 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  // any object should have a unique identifier
+
+  constructor(coords, distance, duration) {
+    // this.date = ...
+    // this.id = ...
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration); // super class with common arguments from parent class will initialize the this keyword
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    // this.pace ads this new property
+    // return this.pace // instead of returning, call in the constructor
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration); // super class with common arguments from parent class will initialize the this keyword
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km / h
+    this.speed = this.distance / (this.duration / 60);
+    // return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycle1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycle1);
+
+///////////////////////////////////////
+// APPLICATION ARCHITECTURE
 class App {
   #map; // define these variables in the App class; related to the app object;
   #mapEvent; // both become private instance properties; better than using as global varoable
